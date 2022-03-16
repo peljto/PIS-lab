baffb57150c18a9120c9f4340ee1f66536034926
Matej Kožica <39216604+MatejKozica@users.noreply.github.com>
2022-03-16 17:37:37 +0100

    Kozy's message

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("EJJJ!");
+            Console.WriteLine("Ja sam prominia!");
         }
     }
 }
